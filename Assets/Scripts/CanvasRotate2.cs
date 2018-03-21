1ff19c090a5ca0b78ca2782f47f8a7b49fe5c155
Diogo Ferreira Martins <diogofm99@gmail.com>
2018-03-21 03:26:26 +0000

    Improved Rotating Canvas Script - 95% Complete

--- Assets/Scripts/CanvasRotate2.cs
+++ Assets/Scripts/CanvasRotate2.cs
@@ -11,6 +11,9 @@ public class CanvasRotate2 : MonoBehaviour
     public int keyCount;
     public float speed = 3f;
     public float angleY;
+    public float timer;
+    private float resetTimer;
+    public float angle = 90;
     // Use this for initialization
     void Start()
     {
@@ -20,6 +23,10 @@ public class CanvasRotate2 : MonoBehaviour
         rotation = Quaternion.LookRotation(lookPos);
         rotation *= Quaternion.Euler(0, -90, 0); // this adds a 90 degrees Y rotation
         angleY = myTransform.rotation.eulerAngles.y;
+        resetTimer = timer;
+    }
+    private void FixedUpdate()
+    {
     }
 
     // Update is called once per frame
@@ -27,19 +34,40 @@ public class CanvasRotate2 : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Q))
             keyCount = 1;
-        {
+            angleY = myTransform.rotation.eulerAngles.y;
             if (keyCount == 1)
             {
+                timer -= Time.deltaTime;
                 transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * speed);
-                if (angleY >= 89.9 && angleY <= 90.1)
+                if (timer <= 0)
                 {
                     keyCount = 0;
+                    timer = resetTimer;
+                    lookPos = myTransform.position - transform.position;
+                    lookPos.y = 0;
+                    rotation = Quaternion.LookRotation(lookPos);
+                    if (angleY >= -1 && angleY <= 1)
+                    {
+                        angle = -90;
+                    }
+                    if (angleY >=269 && angleY <= 271)
+                    {
+                        angle = -180;
+                    }
+                    if (angleY >= 179 && angleY <= 181)
+                    {
+                        angle = -270;
+                    }
+                    if (angleY >= 89 && angleY <= 91)
+                    {
+                        angle = -360;
+                    }
+                rotation *= Quaternion.Euler(0, angle, 0);
                 }
 
             }
         }
     }
-}
 
 /*
  *  var lookPos = target.position - transform.position;
