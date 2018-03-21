7e3b0315133f6acd2b92372230dbb5fa833710e0
Diogo Ferreira Martins <diogofm99@gmail.com>
2018-03-21 02:41:20 +0000

    New Canvas Rotating Script - Incomplete

---
+++ Assets/Scripts/CanvasRotate2.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasRotate2 : MonoBehaviour
+{
+    public GameObject myGO;
+    public Transform myTransform;
+    public Vector3 lookPos;
+    public Quaternion rotation;
+    public int keyCount;
+    public float speed = 3f;
+    public float angleY;
+    // Use this for initialization
+    void Start()
+    {
+        myTransform = myGO.transform;
+        lookPos = myTransform.position - transform.position;
+        lookPos.y = 0;
+        rotation = Quaternion.LookRotation(lookPos);
+        rotation *= Quaternion.Euler(0, -90, 0); // this adds a 90 degrees Y rotation
+        angleY = myTransform.rotation.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            keyCount = 1;
+        {
+            if (keyCount == 1)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * speed);
+                if (angleY >= 89.9 && angleY <= 90.1)
+                {
+                    keyCount = 0;
+                }
+
+            }
+        }
+    }
+}
+
+/*
+ *  var lookPos = target.position - transform.position;
+ lookPos.y = 0;
+ var rotation = Quaternion.LookRotation(lookPos);
+ rotation *= Quaternion.Euler(0, 90, 0); // this adds a 90 degrees Y rotation
+ var adjustRotation = transform.rotation.y + rotationAdjust; //<- this is wrong!
+ transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+ */
--- Assets/Scripts/CanvasRotation.cs
+++ Assets/Scripts/CanvasRotation.cs
@@ -2,54 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CanvasRotation : MonoBehaviour {
+public class CanvasRotation : MonoBehaviour
+{
 
     public GameObject canvas;
-    public int keyCount = 0;
+    public int keyCount;
     private Transform canvasRotation;
-    public float speed;
-	// Use this for initialization
-	void Start () {
-        canvasRotation = canvas.transform;
-    }
-	
-	// Update is called once per frame
-	void Update ()
+    private Transform rotationAngle;
+    public float speed = -3;
+    private Quaternion startRot;
+    private Quaternion endRot;
+    public float angleX, angleY, angleZ;
+    // Use this for initialization
+    void Start()
     {
+        //rotationAngle = Quaternion.AngleAxis(-90, Vector3.up);
+        keyCount = 0;
+        canvasRotation = canvas.transform;
+        //startRot = Quaternion.LookRotation(transform.forward);
+        //endRot = Quaternion.LookRotation(transform.right);
+        //angleX = canvasRotation.rotation.eulerAngles.x;
+        angleY = canvasRotation.rotation.eulerAngles.y;
+        //angleZ = canvasRotation.rotation.eulerAngles.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotationAngle = Quaternion.AngleAxis(-90, Vector3.up * Time.deltaTime * speed);
         if (Input.GetKey(KeyCode.Q))
         {
             keyCount = 1;
-            if (keyCount == 1 && canvasRotation.rotation.y <= 0 && canvasRotation.rotation.y > -90)
+            if (keyCount == 1 && angleY <= 0 && angleY > -90)
             {
 
-                canvasRotation.Rotate(0, -speed, 0);
-                if (canvasRotation.rotation.y == -90)
+                //canvasRotation.Rotate(0, -speed, 0);
+                canvasRotation.Rotate(Vector3.up * -speed);
+                //canvasRotation.Rotate(Vector3.up * speeed * Time.deltaTime , Space.World);
+                //canvasRotation = Quaternion.Slerp(canvasRotation, rotationAngle, Time.time * speed);
+                //transform.rotation = Quaternion.Slerp(startRot, endRot, Time.time * speed);
+                //canvasRotation = rotationAngle;
+
+                Debug.Log("Está tudo bem 1");
+                if (angleY <= -90)
                 {
                     keyCount = 0;
                 }
             }
-            if (keyCount == 1 && canvasRotation.rotation.y <= -90 && canvasRotation.rotation.y > -180)
+            if (keyCount == 1 && angleY <= -90 && angleY > -180)
             {
-                canvasRotation.Rotate(0, -speed, 0);
+                canvasRotation.Rotate(Vector3.up * -speed);
                 if (canvasRotation.rotation.y == -180)
                 {
                     keyCount = 0;
                 }
 
             }
-            if (keyCount == 1 && canvasRotation.rotation.y == -180 && canvasRotation.rotation.y > -270)
+            if (keyCount == 1 && angleY == -180 && angleY > -270)
             {
-                canvasRotation.Rotate(0, -speed, 0);
+                canvasRotation.Rotate(Vector3.up * -speed);
                 if (canvasRotation.rotation.y == -270)
                 {
                     keyCount = 0;
                 }
 
             }
-            if (keyCount == 1 && canvasRotation.rotation.y == -270 && canvasRotation.rotation.y > -360)
+            if (keyCount == 1 && angleY == -270 && angleY > -360)
             {
-                canvasRotation.Rotate(0, -speed, 0);
-                if (canvasRotation.rotation.y == -360)
+                canvasRotation.Rotate(Vector3.up * -speed);
+                if (angleY == -360)
                 {
                     keyCount = 0;
                     canvasRotation.Rotate(0, 360, 0);       //Reset canvas rotation to 0
@@ -58,5 +79,54 @@ public class CanvasRotation : MonoBehaviour {
 
             }
         }
-	}
+        if (Input.GetKey(KeyCode.E))
+        {
+            keyCount = 1;
+            if (keyCount == 1 && angleY <= 0 && angleY > -90)
+            {
+
+                //canvasRotation.Rotate(0, -speed, 0);
+                canvasRotation.Rotate(Vector3.up * speed);
+                //canvasRotation.Rotate(Vector3.up * speeed * Time.deltaTime , Space.World);
+                //canvasRotation = Quaternion.Slerp(canvasRotation, rotationAngle, Time.time * speed);
+                //transform.rotation = Quaternion.Slerp(startRot, endRot, Time.time * speed);
+                //canvasRotation = rotationAngle;
+
+                Debug.Log("Está tudo bem 1");
+                if (angleY <= -90)
+                {
+                    keyCount = 0;
+                }
+            }
+            if (keyCount == 1 && angleY <= -90 && angleY > -180)
+            {
+                canvasRotation.Rotate(Vector3.up * speed);
+                if (canvasRotation.rotation.y == -180)
+                {
+                    keyCount = 0;
+                }
+
+            }
+            if (keyCount == 1 && angleY == -180 && angleY > -270)
+            {
+                canvasRotation.Rotate(Vector3.up * speed);
+                if (canvasRotation.rotation.y == -270)
+                {
+                    keyCount = 0;
+                }
+
+            }
+            if (keyCount == 1 && angleY == -270 && angleY > -360)
+            {
+                canvasRotation.Rotate(Vector3.up * speed);
+                if (angleY == -360)
+                {
+                    keyCount = 0;
+                    canvasRotation.Rotate(0, 360, 0);       //Reset canvas rotation to 0
+                }
+                keyCount = 0;
+
+            }
+        }
+    }
 }
