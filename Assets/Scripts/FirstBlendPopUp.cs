1a19751dda32a2816c6ebee5ec299d29797f2f55
Diogo Ferreira Martins <diogofm99@gmail.com>
2018-05-28 22:12:40 +0100

    SpaceButton interface change; BugFixes

--- Assets/Scripts/FirstBlendPopUp.cs
+++ Assets/Scripts/FirstBlendPopUp.cs
@@ -6,11 +6,24 @@ public class FirstBlendPopUp : MonoBehaviour
 {
 
     public float popUpTime = 0.4f;
+    public float fadeInTime = 1f;
+    public float fadeOutTime = 0.5f;
+
     private float timer = 0f;
+    private GameObject spaceButton;
+    private SpriteRenderer sprite;
+    private Color sColor;
+    private bool goAway = false;
+    private int counter;
 
     // Use this for initialization
     void Start()
     {
+        spaceButton = GameObject.FindGameObjectWithTag("SpaceButton");
+        sprite = spaceButton.GetComponent<SpriteRenderer>();
+        sColor = sprite.color;
+        sColor.a = 0;
+        sprite.color = sColor;
     }
 
     // Update is called once per frame
@@ -26,23 +39,54 @@ public class FirstBlendPopUp : MonoBehaviour
     {
 
         if (timer < popUpTime) timer += Time.deltaTime;
-        if (timer < popUpTime)
+        if (timer >= popUpTime)
         {
-            Time.timeScale = 0f;
-            transform.Find("OopsBlend").gameObject.SetActive(true);
+            //Time.timeScale = 1f;
+            sprite.enabled = true;
+            if(!goAway) FadeEffect();
 
-            if (Input.anyKeyDown)
+            if (Input.GetKeyUp(KeyCode.Space))
             {
-                StopPopUp();
+                goAway = true;
+            }
+            if (goAway) StopPopUp();
+        }
+    }
+
+    private void FadeEffect()
+    {
+        if (sColor.a >= 0 && sColor.a <= 1 && counter == 0)
+        {
+            sColor.a += fadeInTime / 100;
+            sprite.color = sColor;
+            if (sColor.a >= 0.9f)
+            {
+                sColor.a = 1;
+                counter++;
             }
         }
 
+        if (sColor.a >= 0.4 && sColor.a <= 1 && counter == 1)
+        {
+            sColor.a += fadeInTime / 100;
+            sprite.color = sColor;
+        }
     }
 
     private void StopPopUp()
     {
-        Time.timeScale = 1f;
-        transform.Find("OopsBlend").gameObject.SetActive(false);
-        BlendCharacters.playerBlended = 2;
+
+        if (sColor.a >= -1)
+        {
+            sColor.a -= fadeOutTime / 100;
+            sprite.color = sColor;
+            if (sColor.a <= 0)
+            {
+                //Time.timeScale = 1f;
+                sprite.enabled = false;
+                BlendCharacters.playerBlended = 2;
+            }
+        }
+
     }
 }
